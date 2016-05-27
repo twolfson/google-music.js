e1638670d26a25c3834b694d0e453c8563637e45
Samuel Attard <samuel.r.attard@gmail.com>
2016-05-27 17:29:42 +1000

    Implement queue methods and events

---
+++ test/queue.js
@@ -0,0 +1,59 @@
+// Load in dependencies
+var expect = require('chai').expect;
+var browserUtils = require('./utils/browser');
+var browserMusicUtils = require('./utils/browser-music');
+
+// Start our tests
+describe.only('A new session with Google Music', function () {
+  browserUtils.openMusic({
+    testName: 'Queue test'
+  });
+
+  describe('when the queue is empty', function () {
+    browserUtils.execute(function getPlaybackState () {
+      return window.gmusic.queue.getSongs();
+    });
+
+    it('should report an empty queue', function () {
+      expect(this.result.length).to.equal(0);
+    });
+  });
+
+  describe('when we are playing music', function () {
+    browserUtils.execute(function setupQueueWatcher () {
+      window.gmusic.on('change:queue', function saveMode (newQueue) {
+        window.queue = newQueue;
+      });
+    });
+    browserMusicUtils.playAnything();
+    browserMusicUtils.waitForPlaybackStart();
+    browserUtils.execute(function getPlaybackState () {
+      return window.gmusic.queue.getSongs();
+    });
+
+    it('should report a non-empty queue', function () {
+      expect(this.result.length).to.not.equal(0);
+    });
+
+    it('should have fired the change event', function () {
+      browserUtils.execute(function getPlaybackState () {
+        return window.queue;
+      });
+      expect(this.result).to.be.a('array');
+      expect(this.result.length).to.not.equal(0);
+    });
+
+    describe('when we clear the queue', function () {
+      browserUtils.execute(function getPlaybackState () {
+        window.gmusic.queue.clear();
+      });
+      browserUtils.execute(function getPlaybackState () {
+        return window.gmusic.queue.getSongs();
+      });
+
+      it('should report an empty queue', function () {
+        expect(this.result.length).to.equal(0);
+      });
+    });
+  });
+});
